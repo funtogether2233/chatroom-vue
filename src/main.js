ed5a190f84844860f3f4313db6bcbe123ed313a1
LiChen <2446432344@qq.com>
2022-11-06 15:29:46 +0800

    引入elementplus

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
 
-createApp(App).mount('#app')
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
+
+import "./style.css";
+import App from "./App.vue";
+
+const app = createApp(App);
+
+app.use(ElementPlus, { size: "small", zIndex: 3000 });
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
+}
+
+app.mount("#app");
